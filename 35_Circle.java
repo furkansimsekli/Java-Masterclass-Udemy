9608fa89b8e1f09e357499e093e895766dcd6202
furkansimsekli <simseklifurkan0@gmail.com>
2021-10-27 01:51:01 +0300

    35-40 Solutions added

---
+++ 35_Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+
+        if(radius < 0)
+            this.radius = 0;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
